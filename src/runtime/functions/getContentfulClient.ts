435d6290912468ae9306b8a25536f49ad292d5ee
Christian Fernandes <christian@casebeat.com>
2024-05-05 16:53:56 +0200

    typo

--- src/runtime/functions/getContentfulClient.ts
+++ src/runtime/functions/getContentfulClient.ts
@@ -3,10 +3,10 @@ import { useRuntimeConfig } from '#imports';
 
 export function getContentfulClient() : contentful.ContentfulClientApi<undefined> {
   const options = useRuntimeConfig().nuxtContentfulPages;
-
+  
   const client = contentful.createClient({
     // This is the space ID. A space is like a project folder in Contentful terms
-    space: options.space,
+    space: options.spaceId,
     // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
     accessToken: options.accessToken,
   });
