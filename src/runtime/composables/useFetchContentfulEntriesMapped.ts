b1130af99da4c00acb920eeb9f1c0ff22094f1d5
Christian Fernandes <christian@casebeat.com>
2024-06-30 15:21:28 +0200

    fix: added missing import

--- src/runtime/composables/useFetchContentfulEntriesMapped.ts
+++ src/runtime/composables/useFetchContentfulEntriesMapped.ts
@@ -1,7 +1,6 @@
-// import { useFetch, mapEntryFieldsToPage } from '#imports'
+ import { useFetch } from '#imports'
 
 export default async function useFetchContentfulEntriesMapped<T>(contentType: string, offset: number = 0, limit: number = 20) {
-  // get type of T
 
   const query = `?contentType=${contentType}&offset=${offset}&limit=${limit}`
 
@@ -11,16 +10,6 @@ export default async function useFetchContentfulEntriesMapped<T>(contentType: st
   const { data } = await useFetch(url)
 
   const response = data?.value
-
-  // console.log('useFetchContentfulEntriesMapped.response', response);
-  // console.log('useFetchContentfulEntriesMapped.response.items.length', response?.length);
-
-  // if (!response || !response.items) {
-  //   return []
-  // }
-  // const items = response.items.map(item => mapEntryFieldsToPage<T>(item))
-
-  // const items = response.items;
 
   // return items
   return response as T[]
